4eb1d217a85c818c6c4be2e1539532b282806407
Ryne Merritt <rynemerritt@gmail.com>
2024-02-01 10:26:44 -0500

    added keybind, WORKS
    Added Hotkey, updated LICENSE.txt

--- pythonReadOnlyProgram.py
+++ pythonReadOnlyProgram.py
@@ -1,15 +1,27 @@
 import os
 import glob
+import sys
 import stat
+import keyboard
 
+Ticker = 0
 list_of_files = glob.glob(r'C:\Users\rynej\Documents\dataset examples\*')
 latest_file = max(list_of_files, key=os.path.getctime)
 
+keyBind = input("What key would you like to set to enable/disable the read-only?")
+
 File = r"C:\Users\rynej\Documents\dataset examples\example.txt"
-choice = input("1: Enable Read-Only\n2: Disable Read Only\n")
-if choice == "1":
-    print("The file is now read only!")
-    os.chmod(latest_file, stat.S_IREAD)
-elif choice == "2":
-    print("The file is now NOT read only!")
-    os.chmod(latest_file, stat.S_IWRITE)
+
+def Change():
+    global Ticker
+    Ticker += 1
+    choice = Ticker % 2
+    if choice == 1:
+        print("The file is now read only!")
+        os.chmod(latest_file, stat.S_IREAD)
+    elif choice == 0:
+        print("The file is now NOT read only!")
+        os.chmod(latest_file, stat.S_IWRITE)
+
+keyboard.add_hotkey(keyBind, lambda: Change())
+keyboard.wait()
